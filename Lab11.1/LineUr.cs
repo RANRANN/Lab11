a7ea67374d7f85e0639ba164d1d9668ef92f4bd1
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-28 00:03:32 +0300

    Добавьте файлы проекта.

---
+++ Lab11.1/LineUr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._1
+{
+    struct Count
+    {
+        double b;
+        double k;
+
+        public Count(double b, double k)
+        {
+            this.b = b;
+            this.k = k;
+        }
+        public string Root()
+        {
+            double x=-b/k;
+            if (k==0)
+                return "Деление на ноль";
+            return ($"Решение линейного уравнения: x = {x}");
+        }
+    }
+}
---
+++ Lab11.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = Convert.ToDouble(Console.ReadLine());
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Count count = new Count(b,k);
+            Console.WriteLine(count.Root());
+            Console.ReadKey();
+        }
+    }
+}
